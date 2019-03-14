136e4f0dce119de9aaa1b4c7f82639b46f4293d4
Davis <davisperchik@gmail.com>
2019-03-13 20:21:45 -0400

    all tests pass

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,62 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<I> {
+    I[] answerArray;
+
+    public ArrayUtility(I[] inputArray) {
+        answerArray = inputArray;
+    }
+
+    public Integer countDuplicatesInMerge(I[] arrayToMerge, I valueToEvaluate) {
+        Integer intValue = 0;
+        Stream<I> stream1 = Stream.of(answerArray);
+        Stream<I> stream2 = Stream.of(arrayToMerge);
+        Long longAnswer = Stream.concat(stream1, stream2)
+                .filter(s -> valueToEvaluate.equals(s))
+                .count();
+        return intValue = longAnswer.intValue();
+    }
+
+    public I[] removeValue(I valueToRemove) {
+         List<I> list = Arrays.stream(answerArray)
+                .filter(s -> !valueToRemove.equals(s))
+                 .collect(Collectors.toList());
+         I[] result = (I[]) Array.newInstance(answerArray.getClass().getComponentType(),list.size());
+
+        return list.toArray(result);
+    }
+
+    public I getMostCommonFromMerge(I[] arrayToMerge) {
+        I answer = null;
+        Integer counter = 0;
+        Stream<I> stream1 = Stream.of(answerArray);
+        Stream<I> stream2 = Stream.of(arrayToMerge);
+        List<I> list = Stream.concat(stream1, stream2)
+                .collect(Collectors.toList());
+        for (I object : list){
+            if (getNumberOfOccurrences(object) > counter){
+                answer = object;
+                counter = getNumberOfOccurrences(object);
+            }
+        }
+        return answer;
+    }
+
+    public Integer getNumberOfOccurrences(I valueToEvaluate) {
+        Integer intValue = 0;
+        Long answer = Arrays.stream(answerArray)
+                .filter(s -> valueToEvaluate.equals(s))
+                .count();
+        return intValue = answer.intValue();
+    }
 }
